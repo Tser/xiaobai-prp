0899d57c2d1901c1401391eb39a989858f93e6b2
xiaohui <807447312@qq.com>
2026-01-28 18:26:53 +0800

    add test

---
+++ test/test_prp.py
@@ -0,0 +1,34 @@
+import pytest
+
+from prp import PRP
+
+
+def test_add_registry():
+    """
+    测试添加索引源
+    """
+    prp = PRP()
+    prp.add_registry("pypi", "")
+    assert prp.list_registries() == [
+        {"name": "pypi", "url": "", "home": ""}
+    ]
+
+def test_delete_registry():
+    """
+    测试删除索引源
+    """
+    prp = PRP()
+    prp.add_registry("pypi", "")
+    prp.delete_registry("pypi")
+    assert prp.list_registries() == []
+
+def test_use_registry():
+    """
+    测试使用索引源
+    """
+    prp = PRP()
+    prp.add_registry("pypi", "")
+    prp.use_registry("pypi")
+    assert prp.current_registry_info() == {
+        "name": "pypi", "url": "", "home": ""
+    }
